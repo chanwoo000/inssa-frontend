f91a5102d0ddb8711f1bbf8e45af92e0ce7a2e9f
chanwoo <chanwoo0607@naver.com>
2024-07-24 12:05:40 +0900

    feat: 1차 커밋

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Decorate from './pages/Decorate'
+import ImageUpload from './pages/ImageUpload'
+import MyPage from './pages/MyPage'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Splash from './pages/Splash'
+import Theme from './pages/Theme'
+import Footer from './components/footer/Footer'
+import Header from './components/header/Header'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='App'>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Splash />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/my-page' element={<MyPage />} />
+          <Route path='/theme' element={<Theme />} />
+          <Route path='/image-upload' element={<ImageUpload />} />
+          <Route path='/decorate' element={<Decorate />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

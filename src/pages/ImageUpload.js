040c2cbdaa404a6a54defacd92de2e0df36ccc95
chanwoo <chanwoo0607@naver.com>
2024-08-17 16:19:22 +0900

    refactor : 코드 수정

--- src/pages/ImageUpload.js
+++ src/pages/ImageUpload.js
@@ -1,82 +1,8 @@
 import React, { useState, useRef } from 'react'
 import { useNavigate } from 'react-router-dom'
-import styled from 'styled-components'
 import backGroundImage from '../assets/backGroundIamge.svg'
-import Button from '../components/Button/MainButton'
-
-const ImagePreview = styled.img`
-  max-width: 120px;
-  max-height: 150px;
-  cursor: pointer; /* Add pointer cursor to indicate it's clickable */
-`
-
-const ImageList = styled.div`
-  display: grid;
-  position: fixed;
-  grid-template-columns: repeat(3, 1fr);
-  gap: 16px;
-  margin-top: 100px;
-  max-height: 600px;
-  overflow-y: scroll;
-  &::-webkit-scrollbar {
-    display: none;
-  }
-`
-
-const ImageItem = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`
-
-const FolderName = styled.div`
-  font-size: 16px;
-  margin-top: 10px;
-`
-
-const InputContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`
-
-const Input = styled.input`
-  margin-bottom: 10px;
-  padding: 8px;
-  border-radius: 5px;
-  border: 1px solid #ccc;
-  font-size: 16px;
-`
-
-const ModalBackground = styled.div`
-  display: ${(props) => (props.show ? 'flex' : 'none')};
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.5);
-  justify-content: center;
-  align-items: center;
-`
-
-const ModalContent = styled.div`
-  background: white;
-  padding: 20px;
-  border-radius: 10px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  color: black;
-`
-
-const ModalButton = styled.button`
-  background-color: white;
-  border: none;
-  cursor: pointer;
-  width: 50px;
-  height: 30px;
-`
+import Button from '../components/button/MainButton'
+import * as style from '../components/imageupload/ImageUploadStyle'
 
 export default function ImageUpload() {
   const navigate = useNavigate()
@@ -138,35 +64,35 @@ export default function ImageUpload() {
         onChange={handleFileChange}
       />
       <Button onClick={handleButtonClick}>이미지 추가하기</Button>
-      <ImageList>
+      <style.ImageList>
         {images.map(
           (image) =>
             image.src && (
-              <ImageItem key={image.id}>
-                <ImagePreview
+              <style.ImageItem key={image.id}>
+                <style.ImagePreview
                   src={image.src}
                   alt={image.note}
                   onClick={() => handleImageClick(image)}
                 />
-                <FolderName>{image.note}</FolderName>
+                <style.FolderName>{image.note}</style.FolderName>
                 <div>{image.date}</div>
-              </ImageItem>
+              </style.ImageItem>
             )
         )}
-      </ImageList>
+      </style.ImageList>
 
-      <ModalBackground show={modalOpen}>
-        <ModalContent>
+      <style.ModalBackground show={modalOpen}>
+        <style.ModalContent>
           <h4>이미지 정보 입력</h4>
-          <InputContainer>
-            <Input
+          <style.InputContainer>
+            <style.Input
               type='date'
               value={newImage.date}
               onChange={(e) =>
                 setNewImage({ ...newImage, date: e.target.value })
               }
             />
-            <Input
+            <style.Input
               type='text'
               placeholder='한 줄 메모'
               value={newImage.note}
@@ -174,11 +100,11 @@ export default function ImageUpload() {
                 setNewImage({ ...newImage, note: e.target.value })
               }
             />
-          </InputContainer>
-          <ModalButton onClick={handleSave}>저장</ModalButton>
-          <ModalButton onClick={closeModal}>닫기</ModalButton>
-        </ModalContent>
-      </ModalBackground>
+          </style.InputContainer>
+          <style.ModalButton onClick={handleSave}>저장</style.ModalButton>
+          <style.ModalButton onClick={closeModal}>닫기</style.ModalButton>
+        </style.ModalContent>
+      </style.ModalBackground>
     </div>
   )
 }
--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -1,65 +1,9 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import styled from 'styled-components'
 import Modal from 'react-modal'
 import folderImage from '../assets/folder.svg'
-import Button from '../components/Button/MainButton'
-
-const FolderContainer = styled.div`
-  display: grid;
-  position: fixed;
-  top: 100px;
-  grid-template-columns: repeat(3, 1fr);
-  gap: 16px;
-  padding: 16px;
-`
-
-const Folder = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 16px;
-  gap: 10px;
-`
-
-const FolderName = styled.div`
-  font-size: 16px;
-`
-
-const customStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    transform: 'translate(-50%, -50%)',
-    background: `url(${folderImage}) no-repeat center center`,
-    backgroundSize: 'cover',
-    width: '200px', // 폴더 이미지 크기에 맞게 조정
-    height: '200px', // 폴더 이미지 크기에 맞게 조정
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    gap: '30px'
-  }
-}
-
-const ButtonContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  gap: 10px;
-`
-const ModalButton = styled.button`
-  background-color: white;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  width: 50px;
-  height: 30px;
-`
-
-Modal.setAppElement('#root')
+import Button from '../components/button/MainButton'
+import * as style from '../components/mypage/MyPageStyle'
 
 const MyPage = () => {
   const [folders, setFolders] = useState([
@@ -95,21 +39,21 @@ const MyPage = () => {
 
   return (
     <div className='container'>
-      <FolderContainer>
+      <style.FolderContainer>
         {folders.map((folder) => (
           <div key={folder.id} onClick={() => handleFolderClick(folder.id)}>
-            <Folder key={folder.id}>
+            <style.Folder key={folder.id}>
               <img src={folderImage} alt='Folder' />
-              <FolderName>{folder.name}</FolderName>
-            </Folder>
+              <style.FolderName>{folder.name}</style.FolderName>
+            </style.Folder>
           </div>
         ))}
-      </FolderContainer>
+      </style.FolderContainer>
       <Button onClick={openModal}>폴더 추가하기</Button>
       <Modal
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
-        style={customStyles}
+        style={style.customStyles}
       >
         <input
           type='text'
@@ -125,10 +69,10 @@ const MyPage = () => {
             textAlign: 'center'
           }}
         />
-        <ButtonContainer>
-          <ModalButton onClick={handleAddFolder}>추가</ModalButton>
-          <ModalButton onClick={closeModal}>닫기</ModalButton>
-        </ButtonContainer>
+        <style.ButtonContainer>
+          <style.ModalButton onClick={handleAddFolder}>추가</style.ModalButton>
+          <style.ModalButton onClick={closeModal}>닫기</style.ModalButton>
+        </style.ButtonContainer>
       </Modal>
     </div>
   )
